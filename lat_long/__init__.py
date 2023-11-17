6fd26f2dfe0977909b7a037cd45229d7b1fc9bf8
phaeseval <dhanut@tuta.io>
2023-11-17 15:58:46 +0530

    making minor tweaks and adding error handling

--- lat_long/__init__.py
+++ lat_long/__init__.py
@@ -5,9 +5,7 @@ from urllib.request import urlopen
 # Defining a function to get the latitude and longitude of a given city
 def get_lat_long(city):
    # Creating an instance of the Nominatim class: without this geocode fails
-   geolocator = Nominatim(user_agent="myGeocoder")
-   # get precise location using maual entry or gps
-   location = geolocator.geocode(city)
+   location = Nominatim(user_agent="testapp").geocode(city)
    return location.latitude, location.longitude
 
 def get_coarse_location():
@@ -16,3 +14,4 @@ def get_coarse_location():
    ip_latitude = data['loc'].split(',')[0]
    ip_longitude = data['loc'].split(',')[1]
    return ip_latitude, ip_longitude
+
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask, jsonify, request, render_template
+from werkzeug.exceptions import HTTPException
+import json
 
 # Importing local modules that forecasts weather and provides latitude and longitude
 import lat_long
@@ -31,6 +33,18 @@ def weather_data_coarse():
     return jsonify(weather_result)
   else:
     return render_template('index.html')
+
+# Handling errors
+@app.errorhandler(Exception)
+def handle_exceoption(error):
+  response = error.get_response()
+  response.data = json.dumps({
+    "code": error.code,
+    "name": error.name,
+    "description": error.description,
+  })
+  response.content_type = "application/json"
+  return response
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0") # Running the Flask application on the local network
