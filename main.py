88c61a3629dec3bcb8c5ae1806179b9093cc7717
phaeseval <dhanut@tuta.io>
2023-11-21 14:50:43 +0530

    calling proper classes & improvs on frontend conns

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, jsonify, request, render_template, Response
 from flask_cors import CORS
 from werkzeug.exceptions import HTTPException
 import json
@@ -6,10 +6,15 @@ import time
 
 # Importing local modules that forecasts weather and provides latitude and longitude
 from location import GetLocation
-import forecast
+from forecast import Forecast
 
 app = Flask(__name__)
 CORS(app)
+
+@app.before_request
+def handle_options():
+   if request.method == 'OPTIONS':
+       return Response(status=200)
 
 # Flask decorator for homepage and html links
 @app.route('/')
@@ -17,12 +22,12 @@ def index():
   return jsonify({'message':'welcome to the weatherapp api'})
 
 # Function to get input from form and return forecast in json
-@app.route('/submit/<city>', methods=["POST"])
+@app.route('/submit', methods=["POST"])
 def weather_data_precise(): 
   try:
     city = request.form.get('city')
     lat, long = GetLocation(city_name=city).get_cordinates()
-    weather_result = forecast.get_weather(lat, long)
+    weather_result = Forecast(lat=lat, long=long).get_json()
     return jsonify(weather_result)
   except Exception as e:
     app.logger.error(f"An error occurred: {e}")
@@ -34,7 +39,7 @@ def weather_data_coarse():
   try:
     ip_address="8.8.8.8"
     lat, long = GetLocation(ip_address=ip_address).get_cordinates()
-    weather_result = forecast.get_weather(lat, long)
+    weather_result = Forecast(lat=lat, long=long).get_json()
     return jsonify(weather_result)
   except Exception as e:
     app.logger.error(f"An error occurred: {e}")
@@ -49,8 +54,8 @@ def handle_exceoption(error):
     "name": error.name,
     "description": error.description,
   })
-  response.content_type = "application/json"
-  return response
+  app.logger.error(str(error))
+  return Response("An error occurred", status=500)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0") # Running the Flask application on the local network
